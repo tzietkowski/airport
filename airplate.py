02892c9941b339b3a70bafc562bcd92cc8e7b7ba
Tomasz Ziętkowski <tzietkowski@gmail.com>
2021-09-16 11:59:45 +0200

    adding point flying

--- airplate.py
+++ airplate.py
@@ -11,14 +11,15 @@ from random import randint
 class AirPlane():
 
     def __init__(self, flight_number: int) -> None:
-        self.x_pos = randint(0, 10000)
-        self.y_pos = randint(0, 10000)
+        self.x_pos = randint(0, 1000)
+        self.y_pos = randint(0, 1000)
         self.theta = 0
-        self.speed = 1
+        self.__speed = 50
         self.start_time = time.time()
-        self.target = [50, 50]
+        self.__target = (50, 50)
         self.flight_number = flight_number
-        self.last_angle = 0 
+        self.__last_angle = 0
+        self.__the_circle_point_number = 0
         self.q = np.array([[self.x_pos],
                            [self.y_pos],
                            [self.theta]])
@@ -40,36 +41,35 @@ class AirPlane():
             \nOur location x:{str(self.x_pos)}, y:{str(self.y_pos)}.
             \nWe ask for permission to land''')
             time.sleep(1)
-            self.fly(self.target, int(self.start_time - time.time()))
-            print(welcome)
+            self.fly(self.__target)
+            #print(welcome)
             self.client.sendall(str.encode(welcome))
             Response = self.client.recv(1024)
             self.the_circle()
-            print(Response.decode('utf-8'))
+            #print(Response.decode('utf-8'))
 
     def angle_to_target(self, target: List) -> float:
         "Angle to the target"
 
-        fi = math.atan2(target[1] - self.y_pos, -(target[0] - self.x_pos))
-        fi_1 = math.atan2(math.sin(self.last_angle), math.cos(self.last_angle))
-        fi_t_delta = fi - fi_1
-        if fi_t_delta > math.pi:
-            fi_delta = fi_t_delta - (2 * math.pi)
+        angle = math.atan2(target[1] - self.y_pos, -(target[0] - self.x_pos))
+        angle_1 = math.atan2(math.sin(self.__last_angle),
+                             math.cos(self.__last_angle))
+        angle_t_delta = angle - angle_1
+        if angle_t_delta > math.pi:
+            angle_delta = angle_t_delta - (2 * math.pi)
         else:
-            if fi_t_delta < -math.pi:
-                fi_delta = fi_t_delta + (2 * math.pi)
+            if angle_t_delta < -math.pi:
+                angle_delta = angle_t_delta + (2 * math.pi)
             else:
-                fi_delta = fi_t_delta
-        fi = self.last_angle + fi_delta
-        self.last_angle = fi
-        print(fi)
-        return fi
-        # return math.atan2(target[1] - self.y_pos, -(target[0] - self.x_pos))
+                angle_delta = angle_t_delta
+        angle = self.__last_angle + angle_delta
+        self.__last_angle = angle
+        return angle
 
     def speed_to_target(self, target: List) -> np.ndarray:
         "Speed to point calculation"
-        print(self.angle_to_target(target))
-        return self.speed * np.array(
+
+        return self.__speed * np.array(
             [[math.cos(self.angle_to_target(target))],
              [math.sin(self.angle_to_target(target))]])
 
@@ -81,7 +81,7 @@ class AirPlane():
         inv_P = np.linalg.inv(P)
         return np.dot(inv_P, self.speed_to_target(target))
 
-    def fly(self, target: List):
+    def fly(self, target: List) -> None:
         "Calculate position change"
 
         Vx, omega = self.control_speed(target)
@@ -94,19 +94,31 @@ class AirPlane():
     def get_position(self) -> List:
         "get date position samolotu"
 
-        return self.q
+        return (int(self.q[0]), int(self.q[1]))
 
     def disconnect(self) -> None:
-        """Function disconnect to the serwer"""
+        "Function disconnect to the serwer"
 
         print('Closing connection')
         self.client.close()
 
-    def the_circle(self):
-        """Waiting for permission to land """
+    def the_circle(self) -> None:
+        "Waiting for permission to land "
 
+        circle_point = [(100, 100), (100, 900), (900, 900), (900, 100)]
+        
+        self.__target = circle_point[self.__the_circle_point_number]
         pozycja_now = self.get_position()
-        print(pozycja_now)
+        if abs(pozycja_now[0] - self.__target[0]) < 50 and abs(pozycja_now[1] - self.__target[1]) < 50:
+            self.__the_circle_point_number += 1
+            if self.__the_circle_point_number == len(circle_point):
+                self.__the_circle_point_number = 0
+        print(pozycja_now, self.__target)
+
+    def landing(self, approach: tuple, end_point: tuple) -> None:
+        "Landing procedure "
+
+        pass
 
 
 AirPlane(12)
